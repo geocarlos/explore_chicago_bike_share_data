990d6d4c1e1a1a65f8d00e4a30d2c25ef2a30e12
Geocarlos Alves <geowildcat@gmail.com>
2018-11-09 09:20:41 -0400

    Reactor code to meet requirements

--- chicago_bikeshare.py
+++ chicago_bikeshare.py
@@ -52,15 +52,15 @@ for i, row in enumerate(data_list[:20]):
 input("Press Enter to continue...")
 # TASK 3
 # TODO: Create a function to add the columns(features) of a list in another list in the same order
-"""
-def column_to_list(data, index)
-    Params:
+
+def column_to_list(data, index):
+    """
+    Args:
         data: a list containing a dataset.
         index: the index to find a specific column.
     Returns:
         A list of the columns matching the provided index.
-"""
-def column_to_list(data, index):
+    """
     column_list = []
     # Tip: You can use a for to iterate over the samples, get the feature by index and append into a list
     for d in data:
@@ -82,9 +82,8 @@ input("Press Enter to continue...")
 # Now we know how to access the features, let's count how many Males and Females the dataset have
 # TASK 4
 # TODO: Count each gender. You should not use a function to do that.
-male = len([x for x in data_list if x[-2] == 'Male'])
-female = len([x for x in data_list if x[-2] == 'Female'])
-
+male = len([row for row in data_list if row[-2] == 'Male'])
+female = len([row for row in data_list if row[-2] == 'Female'])
 
 # Checking the result
 print("\nTASK 4: Printing how many males and females we found")
@@ -99,17 +98,18 @@ input("Press Enter to continue...")
 # TASK 5
 # TODO: Create a function to count the genders. Return a list
 # Should return a list with [count_male, counf_female] (e.g., [10, 15] means 10 Males, 15 Females)
-"""
-def count_gender(data_list)
-    Param:
+
+def count_gender(data_list):
+    """
+    Args:
         data_list: a list containing a dataset.
     Returns:
         A list containing number of "Males" and number of "Females"
         found in the dataset.
-"""
-def count_gender(data_list):
-    male = len([x for x in data_list if x[-2] == 'Male'])
-    female = len([x for x in data_list if x[-2] == 'Female'])
+    """
+    gender_list = column_to_list(data_list, -2)
+    male = len([gender for gender in gender_list if gender == 'Male'])
+    female = len([gender for gender in gender_list if gender == 'Female'])
     return [male, female]
 
 
@@ -128,18 +128,17 @@ input("Press Enter to continue...")
 # TODO: Create a function to get the most popular gender and print the gender as string.
 # We expect to see "Male", "Female" or "Equal" as answer.
 
-"""
-def most_popular_gender(data_list)
-    Param:
+def most_popular_gender(data_list):
+    """
+    Args:
         data_list: a list containing a dataset.
     Returns:
         String "Equal" if number of "Males" == number of "Females".
         String "Male" if number of "Males" > number of "Females".
         String "Female" if number of "Males" < number of "Females".
-"""
-def most_popular_gender(data_list):
-    dl = count_gender(data_list)
-    answer = "Equal" if dl[0] == dl[1] else ("Male" if dl[0] > dl[1] else "Female")
+    """
+    cg = count_gender(data_list)
+    answer = "Equal" if cg[0] == cg[1] else ("Male" if cg[0] > cg[1] else "Female")
     return answer
 
 
@@ -168,17 +167,14 @@ input("Press Enter to continue...")
 # TODO: Plot a similar graph for user_types. Make sure the legend is correct.
 print("\nTASK 7: Check the chart!")
 user_type_list = column_to_list(data_list, -3)
-u_types = ["Customer", "Subscriber", "Dependent"]
-quantity = [
-        len([x for x in data_list if x[-3] == "Customer"]),
-        len([x for x in data_list if x[-3] == "Subscriber"]),
-        len([x for x in data_list if x[-3] == "Dependent"])
-    ]
-y_pos = list(range(len(u_types)))
+user_types = ["Customer", "Subscriber", "Dependent"]
+quantity = [len([type for type in user_type_list if type == t])
+    for t in user_types]
+y_pos = list(range(len(user_types)))
 plt.bar(y_pos, quantity)
 plt.ylabel('Quantity')
 plt.xlabel('User Type')
-plt.xticks(y_pos, u_types)
+plt.xticks(y_pos, user_types)
 plt.title('Quantity by User Type')
 plt.show(block=True)
 
@@ -202,59 +198,55 @@ input("Press Enter to continue...")
 # You should not use ready functions to do that, like max() or min().
 trip_duration_list = column_to_list(data_list, 2)
 
-"""
-def get_min(number_list)
-    Param:
+def get_min(number_list):
+    """
+    Args:
         number_list: a list of numeric values (they can be string)
     Returns:
         min: the smallest number in number_list as a float
-"""
-def get_min(number_list):
+    """
     min = float(number_list[-1])
-    for n in number_list:
-        if min > float(n):
-            min = float(n)
+    for num in number_list:
+        if min > float(num):
+            min = float(num)
     return min
 
-"""
-def get_max(number_list)
-    Param:
+def get_max(number_list):
+    """
+    Args:
         number_list: a list of numeric values (they can be string)
     Returns:
         max: the largest number in number_list as a float
-"""
-def get_max(number_list):
+    """
     max = 0
-    for n in number_list:
-        if max < float(n):
-            max = float(n)
+    for num in number_list:
+        if max < float(num):
+            max = float(num)
     return max
 
-"""
-def get_mean(number_list)
-    Param:
+def get_mean(number_list):
+    """
+    Args:
         number_list: a list of numeric values (they can be string)
     Returns:
         The arithmetic mean of the numbers in number_list as a float
-"""
-def get_mean(number_list):
+    """
     list_sum = 0
-    for n in number_list:
-        list_sum += float(n)
+    for num in number_list:
+        list_sum += float(num)
     return list_sum / len(number_list)
 
-"""
-def get_median(number_list)
-    Param:
+def get_median(number_list):
+    """
+    Args:
         number_list: a list of numeric values (they can be string)
     Returns:
         The median of the numbers in number_list as a float
-"""
-def get_median(number_list):
-    l = sorted([float(x) for x in number_list])
-    if len(l) % 2 == 0:
-        return float(l[len(l)//2 - 1] + l[len(l)//2]) / 2
-    return float(l[len(l)//2 - 1])
+    """
+    sorted_list = sorted([float(num) for num in number_list])
+    if len(sorted_list) % 2 == 0:
+        return sorted_list[len(sorted_list)//2 - 1] + sorted_list[len(l)//2] / 2
+    return float(sorted_list[len(sorted_list)//2 - 1])
 
 min_trip = get_min(trip_duration_list)
 max_trip = get_max(trip_duration_list)
@@ -275,7 +267,7 @@ input("Press Enter to continue...")
 # TASK 10
 # Gender is easy because usually only have a few options. How about start_stations? How many options does it have?
 # TODO: Check types how many start_stations do we have using set()
-user_types = set([s[3] for s in data_list])
+user_types = set([station for station in column_to_list(data_list, 3)])
 
 print("\nTASK 10: Printing start stations:")
 print(len(user_types))
@@ -306,19 +298,17 @@ input("Press Enter to continue...")
 print("Will you face it?")
 answer = "yes"
 
-"""
-def count_items(column_list)
-    Param:
+def count_items(column_list):
+    """
+    Args:
         A list of a particular column from dataset.
     Returns:
         A tuple of two lists, the one containing all the item types found and
         the other containing counts for each type.
-"""
-def count_items(column_list):
-    item_types = list(set([x for x in column_list]))
-    count_items = []
-    for t in item_types:
-        count_items.append(len([x for x in column_list if x == t]))
+    """
+    item_types = list(set([type for type in column_list]))
+    count_items = [len([type for type in column_list if type == t])
+        for t in item_types]
     return item_types, count_items
 
 
